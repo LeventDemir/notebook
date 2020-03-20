23b0914f55c885939e87896d57f8afad7dbbdc47
LeventDemir <leventdemir518@gmail.com>
2020-03-20 13:43:23 +0300

    Navbar component created

---
+++ components/navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="mobileNavbar"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="mobileNavbar" class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">Home</div>
+          <a class="navbar-item">Create note</a>
+          <a class="navbar-item">Logout</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  mounted() {
+    const $navbarBurgers = Array.prototype.slice.call(
+      document.querySelectorAll(".navbar-burger"),
+      0
+    );
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+      // Add a click event on each of them
+      $navbarBurgers.forEach(el => {
+        el.addEventListener("click", () => {
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle("is-active");
+          $target.classList.toggle("is-active");
+        });
+      });
+    }
+  }
+};
+</script>
