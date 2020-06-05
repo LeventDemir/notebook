9a5249c9d557913534c1e3fb2857114dfb5d40fd
LeventDemir <leventdemir518@gmail.com>
2020-06-05 17:54:13 +0300

    auth middleware created

--- server/routes/note.js
+++ server/routes/note.js
@@ -2,19 +2,54 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const Note = require('../models/note')
+const auth = require('../middleware/auth')
 
 
-router.post('/create-note', (req, res) => {
+router.post('/create-note', auth, (req, res) => {
     const data = req.body
 
-    User.findOne({ token: data.token }, (err, user) => {
-        if (user && user.login) {
-            const date = new Date()
+    const date = new Date()
 
-            data.createdAt = date.toGMTString().split(":")[0] + ":" + date.getMinutes()
-            data.author = user._id
+    data.createdAt = date.toGMTString().split(":")[0] + ":" + date.getMinutes()
 
-            new Note(data).save(err => {
+    new Note(data).save(err => {
+        if (!err) {
+            res.json({ success: true })
+        } else {
+            res.json({ success: false })
+        }
+    })
+})
+
+
+router.post('/update-note', auth, (req, res) => {
+    const data = req.body
+
+    Note.findOne({ _id: data.id, author: data.author }, (err, note) => {
+        if (note) {
+            note.photo = data.photo
+            note.note = data.note
+
+            note.save(err => {
+                if (!err) {
+                    res.json({ success: true })
+                } else {
+                    res.json({ err })
+                }
+            })
+        } else {
+            res.json({ success: false })
+        }
+    })
+})
+
+
+router.post('/delete-note', auth, (req, res) => {
+    const data = req.body
+
+    Note.findOne({ _id: data.id, author: data.author }, (err, note) => {
+        if (note) {
+            note.remove(err => {
                 if (!err) {
                     res.json({ success: true })
                 } else {
@@ -28,71 +63,12 @@ router.post('/create-note', (req, res) => {
 })
 
 
-router.post('/update-note', (req, res) => {
-    const data = req.body
-
-    User.findOne({ token: data.token }, (err, user) => {
-        if (user && user.login) {
-            Note.findOne({ author: user._id, _id: data.id }, (err, note) => {
-                if (note) {
-                    note.photo = data.photo
-                    note.note = data.note
-
-                    note.save(err => {
-                        if (!err) {
-                            res.json({ success: true })
-                        } else {
-                            res.json({ err })
-                        }
-                    })
-                } else {
-                    res.json({ success: false })
-                }
-            })
-        } else {
-            res.json({ success: false })
-        }
-    })
-})
-
-
-router.post('/delete-note', (req, res) => {
-    const data = req.body
-
-    User.findOne({ token: data.token }, (err, user) => {
-        if (user && user.login) {
-            Note.findOne({ _id: data.id, author: user._id }, (err, note) => {
-                if (note) {
-                    note.remove(err => {
-                        if (!err) {
-                            res.json({ success: true })
-                        } else {
-                            res.json({ success: false })
-                        }
-                    })
-                } else {
-                    res.json({ success: false })
-                }
-            })
-        } else {
-            res.json({ success: false })
-        }
-    })
-})
-
-
-router.get('/notes', (req, res) => {
+router.get('/notes', auth, (req, res) => {
     const data = req.query
 
-    User.findOne({ token: data.token }, (err, user) => {
-        if (user && user.login) {
-            Note.find({ author: user._id }, (err, notes) => {
-                if (notes) {
-                    res.json(notes)
-                } else {
-                    res.json({ success: false })
-                }
-            })
+    Note.find({ author: data.author }, (err, notes) => {
+        if (notes) {
+            res.json(notes)
         } else {
             res.json({ success: false })
         }
