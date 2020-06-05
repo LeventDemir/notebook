78605654dd1acdc64f107b14cb5391ecf9202910
LeventDemir <leventdemir518@gmail.com>
2020-06-05 16:42:29 +0300

    update-note route created (api)

--- server/routes/note.js
+++ server/routes/note.js
@@ -14,13 +14,39 @@ router.post('/create-note', (req, res) => {
             data.createdAt = date.toGMTString().split(":")[0] + ":" + date.getMinutes()
             data.author = user._id
 
-            delete data.token
-
             new Note(data).save(err => {
-                if (err) {
-                    res.json(err)
-                } else {
+                if (!err) {
                     res.json({ success: true })
+                } else {
+                    res.json({ success: false })
+                }
+            })
+        } else {
+            res.json({ success: false })
+        }
+    })
+})
+
+
+router.post('/update-note', (req, res) => {
+    const data = req.body
+
+    User.findOne({ token: data.token }, (err, user) => {
+        if (user && user.login) {
+            Note.findOne({ author: user._id, _id: data.id }, (err, note) => {
+                if (note) {
+                    note.photo = data.photo
+                    note.note = data.note
+
+                    note.save(err => {
+                        if (!err) {
+                            res.json({ success: true })
+                        } else {
+                            res.json({ err })
+                        }
+                    })
+                } else {
+                    res.json({ success: false })
                 }
             })
         } else {
