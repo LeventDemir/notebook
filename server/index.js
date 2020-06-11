319ae67ec17054f78c460e3d7499e4e8114a07ef
LeventDemir <leventdemir518@gmail.com>
2020-06-11 16:32:02 +0300

    BD => DB

--- server/index.js
+++ server/index.js
@@ -9,14 +9,14 @@ const note = require('./routes/note')
 const list = require('./routes/list')
 
 
-mongoose.connect(process.env.BD || 'mongodb://localhost/notebook', {
+mongoose.connect(process.env.DB || 'mongodb://localhost/notebook', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 })
 
 mongoose.connection.on("open", () => console.log("✔ Connected to mongodb"));
-mongoose.connection.on("error", err => console.log(err));
+mongoose.connection.on("error", err => console.log({ mongooseError: err }));
 
 
 app.use(bodyParser.json({ limit: '6mb' }))
